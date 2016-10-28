278034da2c7c7c1362a974afed2a5861e03561e9
Roman Malakhov <romaroma@zoom-group.ru>
2016-10-29 06:08:22 +0700

    markers with clustering added

--- js/main.js
+++ js/main.js
@@ -20,3 +20,39 @@ function initMap() {
   mapi.isReady = true;
 }
 
+// Returns lon/lat for a given address
+mapi.prototype.geocode = function(address, callback) {
+  this.geocoder.geocode({
+    'address': address,
+  }, function(results, status) {
+    if (status === google.maps.GeocoderStatus.OK) {
+      typeof callback === "function" && callback(results[0].geometry.location, address);
+    } else {
+      console.log('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+
+// Displays markers with clustering
+mapi.prototype.displayMarkers = function(regions) {
+  var markers = [];
+  for (var i = 0; i < regions.length; i++) {
+    this.geocode(regions[i], function(location, address) {
+      var mark = new google.maps.Marker({
+        position: location,
+        title: address,
+      });
+
+      markers.push(mark);
+
+      // Once we finished - display the markers
+      if (markers.length == regions.length) {
+        // Add a marker clusterer to manage the markers.
+        var markerCluster = new MarkerClusterer(map, markers, {
+          imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
+        });
+      }
+    });
+  }
+}
+
