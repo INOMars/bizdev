bc6209f2cede941a0a59ca22344d8c235be24947
Roman Malakhov <romaroma@zoom-group.ru>
2016-10-29 05:40:56 +0700

    Fixed bug with 0 height of map

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 // Maps are ready to use
 function initMap() {
   console.log('Maps are ready');
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 3,
+    center: {
+      lat: 58,
+      lng: 82
+    }
+  });
+
 }
+
