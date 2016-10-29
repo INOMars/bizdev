e9d625c99ed0139d10a73ce286ba949a1604f330
Roman Malakhov <romaroma@zoom-group.ru>
2016-10-29 07:00:16 +0700

    api call to get list of regions added

--- js/api.js
+++ js/api.js
@@ -3,6 +3,7 @@ var API = function() {
   this.serverUrl = "http://localhost/";
 };
 
+// Ping/pong
 API.prototype.ping = function(callback) {
   var path = "ping",
       actionUrl = this.serverUrl + path,
@@ -11,6 +12,20 @@ API.prototype.ping = function(callback) {
 
   $.ajax(url: actionUrl).done(function() {
     typeof callback === "function" && callback();
+  }).fail(function(){
+    console.log('error');
+  });
+}
+
+// Returns list of regions
+API.prototype.getRegions = function(callback) {
+  var path = "regions",
+      actionUrl = this.serverUrl + path,
+      options = {},
+      self = this;
+
+  $.getJSON(url: actionUrl).done(function(data) {
+    typeof callback === "function" && callback(data);
   }).fail(function(){
     console.log('error');
   });
